07f769d83ff6700b1cd02f3cf697db31d624b67d
flaronin <flaronin1@gmail.com>
2024-10-24 11:21:10 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: "#87CEFA",
+        primaryColorHover: "#00A8FF",
+        secondaryColor: "#121214",
+        thirdColor: "#202024",
+        textPrimaryColor: "#EEEEEE",
+        textSecondaryColor: "",
+      }
+    }
+  },
+  darkMode: 'class',
+}
